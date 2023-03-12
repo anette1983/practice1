aba9520a4f94e35da9805d41e8db9493c2e33f9d
EugeneKom <EugeneKomissarenko@gmail.com>
2023-03-12 19:27:51 +0100

    task4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,7 @@ import {
 import article from '../../data/article.json';
 import data from '../../data/data.json';
 import forbes from '../../data/forbes.json';
+import transactions from '../../data/transactions.json';
 
 export const App = () => {
   return (
@@ -37,12 +38,12 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
         </Heading>
-        <ForbesList list={forbes}/>
+        <ForbesList list={forbes} />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
         </Heading>
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
